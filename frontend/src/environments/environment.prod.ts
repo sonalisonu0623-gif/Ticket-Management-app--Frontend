294e880113c06354c602d6d2e756d382cbce820c
Sonali <sonalisonu0623@gmail.com>
2026-05-15 11:05:06 +0530

    Changed frontend

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://ticket-management-app-backend-4.onrender.com/api'
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://ticket-management-app-backend-4.onrender.com/api'
-};
